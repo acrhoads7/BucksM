2b9035c9db4f2078567aca02aecafe89035470ad
acrhoads7 <32334027+acrhoads7@users.noreply.github.com>
2020-04-28 15:26:24 -0400

    Update BucksM.R

--- R/BucksM.R
+++ R/BucksM.R
@@ -77,6 +77,6 @@ return(p1)
 
 buck.im <- function(model.pred=as.vector(model.pred), data.output=as.matrix(data), m=as.integer(m), n=as.integer(n)){
   data.output[,m] <-ifelse(is.na(data.output[,m]) & (data.output[,ncol(data.output)]==n), model.pred, data.output[,m])
-  return(data[,m])
+  return(data.output[,m])
 }
 
